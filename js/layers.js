994e7d28d76055bb78af97bcebdf213c0ec30dca
Allantyl <86600738+Allantyl@users.noreply.github.com>
2021-06-29 21:18:03 +0800

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -22,7 +22,7 @@ addLayer("p", {
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "p", description: "P: Pack for packages", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "p", description: "P: Pack for packages.", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true},
 	    upgrades: {
@@ -44,6 +44,34 @@ addLayer("p", {
                      cost: new Decimal(100),
                  },
              }, 
-    
+addLayer("f", {
+    name: "factory", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "F", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+    color: "#675C63",
+    requires: new Decimal(100), // Can be a function that takes requirement increases into account
+    resource: "factories", // Name of prestige currency
+    baseResource: "package", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.5, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "f", description: "F: Construct for factories.", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return true}
+})    
 
 })
+
