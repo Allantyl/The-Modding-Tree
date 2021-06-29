0ca82bde7cc62e6b76e0ed9e3f95161fffcf1132
Allantyl <86600738+Allantyl@users.noreply.github.com>
2021-06-29 18:31:07 +0800

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -27,6 +27,9 @@ addLayer("p", {
     layerShown(){return true},
 	    upgrades: {
                  11: {
+                        name: "Make this whatever you want!",
+                        description: "Double your point gain.",
+                        cost: new Decimal(1),
 
         },
 
